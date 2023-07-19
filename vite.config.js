b0777e07d40f713a768f39071227cdaefc534036
Josh H <jhizgiaev2704@gmail.com>
2023-07-19 17:13:07 -0400

    Change build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
 // vite.config.js
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-
+const root = resolve(__dirname, './')
+const outDir = resolve(__dirname, 'dist')
 export default defineConfig({
+  root,
   build: {
+    outDir,
+    emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'pages/about.html'),
-        nested: resolve(__dirname, 'pages/contact.html'),
-        nested: resolve(__dirname, 'pages/projects.html'),
+        main: resolve(root, 'index.html'),
+        contacts: resolve(root, 'pages/contacts','index.html'),
+        about: resolve(root, 'pages/about','index.html'),
+        projects: resolve(root, 'pages/projects','index.html'),
       },
     },
   },
