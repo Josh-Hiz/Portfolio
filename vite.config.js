79d39c9e259012bbb966b206139ba91981cec12c
Josh H <jhizgiaev2704@gmail.com>
2023-07-21 00:17:10 -0400

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,14 @@
 // vite.config.js
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import { terser } from "rollup-plugin-terser";
-
 const root = resolve(__dirname, './')
 const outDir = resolve(__dirname, 'dist')
-
 export default defineConfig({
   root,
   build: {
     outDir,
     emptyOutDir: true,
     rollupOptions: {
-      plugins: [
-        terser() // apply the terser plugin
-      ],
       input: {
         main: resolve(root, 'index.html'),
         contacts: resolve(root, 'pages/contacts','index.html'),
@@ -23,4 +17,4 @@ export default defineConfig({
       },
     },
   },
-});
+})
