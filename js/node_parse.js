a49912496ea4d2785bb7669df2789882fca41ea4
Josh H <jhizgiaev2704@gmail.com>
2023-07-21 17:43:03 -0400

    update link parsing

--- js/node_parse.js
+++ js/node_parse.js
@@ -1,6 +1,6 @@
 var data; 
 var ring = [];
-
+var ringLinks = [];
 async function getNeighbors() {
     try {
         const response = await fetch("https://sitring.eric.si/josh_h/neighbors", {
@@ -15,22 +15,21 @@ async function getNeighbors() {
 
 async function parseNeighbors(){
     data = await getNeighbors();
-    
     for(var i = 0; i < data.length; i++) {
         ring.push(data[i]);
     }
-
     for(var i = 0; i < ring.length; i++) {
         createLinks(ring[i]);
     }
-
 }
 
 function createLinks(data){
     let link = window.document.createElement('a');
     link.href=data.url;
     link.innerText=data.name;
-    console.log(link);
+    link.target="_blank";
+    ringLinks.push(link);
+    console.log(ringLinks);
     window.document.body.append(a);
 }
 
